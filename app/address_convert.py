8864fff52f72a756353e1fd4351a1559e22d9ef2
syankovskiy <semyon21@mail.ru>
2024-01-19 10:22:44 +0300

    house_splitter - разбивает дома

--- app/address_convert.py
+++ app/address_convert.py
@@ -29,9 +29,31 @@ def address_convert(addresses: Tag):
             town = ""
 
         result.append([address,houses])
-
     return result
 
 
 def house_splitter(houses: str) -> List[str]:
-    print("GOT", houses)
+
+    arr = list(houses.split(","))
+    clean_adress = []
+    for item in arr:
+        item = item.replace("(чет)", "")
+        item = item.replace("(нечет)", "")
+        item = item.strip()
+        clean_adress.append(item)
+
+
+    ext_adress = []
+    for item in clean_adress:
+        if '-' in item:
+
+            start, stop = map(str, item.split('-'))
+            if not start.isnumeric() or not stop.isnumeric():
+                ext_adress.append(item)
+                continue
+            for house_num in range(int(start), int(stop) + 1,2):
+                ext_adress.append(f'{house_num}')
+        else:
+            ext_adress.append(item)
+
+    return ext_adress
