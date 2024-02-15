72c5e3f93ef74f2d438a080f16382ba308a2a0db
syankovskiy <semyon21@mail.ru>
2024-02-15 12:08:28 +0300

    Добавлен словарь для одиночных коммутаторов {"Akvamarin": ('Парковая', '11'),} добавлен перевод символов с англ. на рус. в house = f"{house_zabbix + korpus_zabbix}" исправлен тест

--- app/address_to_verbose.py
+++ app/address_to_verbose.py
@@ -131,67 +131,67 @@ dr_dict = {
     "Pobedy": "Победы",
     "Melnika": "Генерала Мельника",
     "Gelovani": "Геловани",
-    #"RELE": " ",
+    # "RELE": " ",
     "Izumrud": "Изумруд",
     "LASPI": "Ласпи",
     "SZ_YUBK": "Севастопольская зона ЮБК",
     "IzumrudAdm": "Изумруд",
-    #"Andreevka": "с. Андреевка",
-    #"Solnechniy": "п. Солнечный",
+    # "Andreevka": "с. Андреевка",
+    # "Solnechniy": "п. Солнечный",
     "Centralnaya": "Центральная",
     "Nevskaya": "Невская",
-    #"Gollandia": " ",
+    # "Gollandia": " ",
     "Blyuhera": "Маршала Блюхера",
     "Flagman": "Флагманская",
-    "MaksDacha": " ",
+    # "MaksDacha": " ",
     "Kalicha": "Калича",
     "Stroitelnaya": "Строительная",
-    #"Ternovka": " ",
+    # "Ternovka": " ",
     "Rabochaya": "Рабочая",
     "Korabelnaya": "Корабельная",
     "Zhidilova": "Генерала Жидилова",
     "Makarova": "Адмирала Макарова",
-    #"Orlinoe": " ",
+    # "Orlinoe": " ",
     "Kedrovaya": "Кедровая",
     "Pahomova": "Ивана Пахомова",
     "Smotrovaya": "Смотровая площадка",
     "Volodarskogo": "Володарского",
     "Popova": "Попова",
     "Sovetskaya": "Советская",
-    #"Kacha": " ",
+    # "Kacha": " ",
     "Aviat": "Авиаторов",
-    #"Komendat": " ",
+    # "Komendat": " ",
     "Krasnoar": "Красноармейская",
     "Nesterova": "Нестерова",
     "Parusk": "Отель Парус",
-    #"Orlovka": " ",
-    #"Belbek": " ",
+    # "Orlovka": " ",
+    # "Belbek": " ",
     "KachinskoeShosse": "Качинское шоссе",
-    #"Lyubimovka": " ",
+    # "Lyubimovka": " ",
     "Shkolnaya": "Школьная",
     "VyazRoscha": "Вязовая роща",
     "STBereg": "Берег",
-    #"Vavilovo": " ",
+    # "Vavilovo": " ",
     "Parshina": "Паршина",
-    #"Verhnesadovoe": " ",
-    #"VerhnesadovoeAdm": " ",
-    #"VerhnesadovoeBiblioteka": " ",
+    # "Verhnesadovoe": " ",
+    # "VerhnesadovoeAdm": " ",
+    # "VerhnesadovoeBiblioteka": " ",
     "Bogdanova": "Богданова",
-    #"BogdMarket": " ",
-    #"SahGolovka": " ",
+    # "BogdMarket": " ",
+    # "SahGolovka": " ",
     "Angarskaya": "Ангарская",
-    #"GRES": " ",
+    # "GRES": " ",
     "Shubikova": "Шубикова",
     "Timiryazeva": "Тимирязева",
     "Tolbuhina": "Толбухина",
     "Odesskaya": "Одесская",
     "GStal": "Героев Сталинграда",
-    "OMEGA": "Омега",
+    # "OMEGA": "Омега",
     "ParkPobedy": "Парк Победы",
     "Yumasheva": "Юмашева",
-    #"Akvamarin": " ",
+    # "Akvamarin": " ",
     "NOstr": "Надежды Островской",
-    #"AhmatovaPark": " ",
+    # "AhmatovaPark": " ",
     "DUlyanova": "Дмитрия Ульянова",
     "Efremova": "Ефремова",
     "Eroshenko": "Ерошенко",
@@ -202,27 +202,50 @@ dr_dict = {
     "Universitet": "Университетская",
     "Konduktor": "Кондукторская",
     "Ohotskaya": "Охотская",
-    #"10KM": "",
+    # "10KM": "",
     "Dneprovskaya": "Днепровская",
     "GOstryakova": "Генерала Острякова",
     "Hrustaleva": "Хрусталёва",
     "Kievskaya": "Киевская",
-    #"Sevenergo": " ",
+    # "Sevenergo": " ",
     "Silaeva": "Силаева",
     "Gasforta": "Гасфорта",
     "Ind": "Индустриальная",
 }
+dr_dict_jr = {
+    "Andreevka": (r'Андреевка(.+?)Центральная', '22'),
+    "Solnechniy": (r'Солнечный(.+?)Андреевская', '21'),
+    "Gollandia": ('Курчатова', '10'),
+    "Ternovka": (r'Терновка(.+?)Ленина', '2'),
+    "Orlinoe": (r'Орлиное(.+?)Тюкова', '66'),
+    "Kacha": (r'Кача(.+?)Авиаторов', '9'),
+    "Orlovka": (r'Осипенко(.+?)Сухий', '1'),
+    "Lyubimovka": (r'Любимовка(.+?)Софьи Перовской', '66'),
+    "Vavilovo": ('Дальнее', '1'),
+    "Verhnesadovoe": (r'Верхнесадовое(.+?)Севастопольская', '66'),
+    "VerhnesadovoeAdm": (r'Верхнесадовое(.+?)Севастопольская', '82'),
+    "VerhnesadovoeBiblioteka": (r'Верхнесадовое(.+?)Севастопольская', '53'),
+    "BogdMarket": ('Богданова', '15'),
+    "SahGolovka": (r'Сахарная Головка(.+?)Трактористов', '9а'),
+    "GRES": ('Яблочкова', '5'),
+    "Akvamarin": ('Парковая', '11'),
+    "10KM": ('пер. Новикова', '1'),
+    "Sevenergo": ('Хрусталёва', '44'),
+    "MaksDacha": ('Каштановая', '3'),
+    "OMEGA": ('Челнокова', '10'),
+    "AhmatovaPark": ('парк Анны Ахматовой', ''),
+
+}
 
 
 def get_info_from_zabbix_node(name: str) -> Tuple[str, str]:
-    #name = name.split("\n")
     res = []
-    street_zabbix = ""
-    house_zabbix = ""
-    korpus_zabbix = ""
+    street = ""
+    house = ""
 
     match = re.search(
-        r"(?:SVSL|FTTB|MSAN)[-_](?:\d+[-_])?(\d{0,2}[a-z_A-Z]+)(\d+[abvwgdeABVWGDE.\d]{0,2})?(k\d{1,2})?\S*", name, flags=re.IGNORECASE)
+        r"(?:SVSL|FTTB|MSAN)[-_](?:\d+[-_])?(\d{0,2}[a-z_A-Z]+)(\d+[abvwgdeABVWGDE.\d]{0,2})?(k\d{1,2})?\S*", name,
+        flags=re.IGNORECASE)
     if match:
         res.append(match.groups())
         # тут разделение на все 3 интересующие нас группы улицу, дом и корпус
@@ -230,15 +253,55 @@ def get_info_from_zabbix_node(name: str) -> Tuple[str, str]:
         house_zabbix = match.group(2)
         korpus_zabbix = match.group(3)
 
+        # костыль, чтобы не менять регулярку
         if street_zabbix == "GOstryakova_" or street_zabbix == "GStalingr_":
-            street_zabbix = street_zabbix.replace("_","")
+            street_zabbix = street_zabbix.replace("_", "")
+        # тут так надо, тот, кто удалит - лох
+        if "Saturn2" in name:
+            return 'Сатурн-2', ''
+        if name == "SVSL-811-Kalicha-ASW1":
+            return 'Калича', '15'
 
-        #print(street_zabbix, house_zabbix, korpus_zabbix)
-
-        if house_zabbix == None:
+        if house_zabbix is None:
             house_zabbix = ""
 
-        if korpus_zabbix == None:
+        if korpus_zabbix is None:
             korpus_zabbix = ""
 
-    return dr_dict.get(street_zabbix), f"{house_zabbix+korpus_zabbix}"
+        house = f"{house_zabbix + korpus_zabbix}"
+        house = house.lower()
+        house = replace_english_with_russian(house)
+
+        street = dr_dict.get(street_zabbix)  # Проверяем улицу в первом словаре dr_dict
+        if street is None:  # Если в первом словаре нет, то берем данные из второго словаря dr_dict_jr
+            res = dr_dict_jr.get(street_zabbix)
+            if res:
+                return res
+            else:
+                return '', ''
+
+    return street, house
+
+
+def replace_english_with_russian(text: str) -> str:
+    # Создаем словарь для замены символов
+    translation_dict = {
+        'a': 'а',
+        'b': 'б',
+        'v': 'в',
+        'g': 'г',
+        'd': 'д',
+        'e': 'е',
+
+        'k': '/',
+    }
+
+    # Проходим по каждому символу в строке и заменяем его, если он есть в словаре
+    result = ''
+    for char in text:
+        if char.lower() in translation_dict:
+            result += translation_dict[char.lower()]
+        else:
+            result += char
+
+    return result
