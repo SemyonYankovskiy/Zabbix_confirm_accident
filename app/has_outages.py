53dfebc77e1f7213d4e3e68aac6d56061388ab90
syankovskiy <semyon21@mail.ru>
2024-02-21 12:09:26 +0300

    Доабвлен тест, функция теперь принимает содержимое json файла, а не сам файл

--- app/has_outages.py
+++ app/has_outages.py
@@ -1,48 +1,54 @@
 import json
 import re
-from datetime import datetime, date
+from datetime import date
 
 
-def has_outages(input_address: tuple, json_file_name: str) -> str:
+def json_opener(json_file_name: str) -> list:
+    try:
+        with open(json_file_name, "r", encoding="utf-8") as json_file:
+            data = json.load(json_file)
+            return data
+    except FileNotFoundError:
+        print("Ошибка: JSON-файл не найден.")
+        return []
+    except json.JSONDecodeError:
+        print("Ошибка: Неверный формат JSON-файла.")
+        return []
+
+
+def has_outages(input_address: tuple, json_file_content: list) -> str:
     """
     Сравнивает входной адрес с адресами из JSON-файла.
 
     Args:
         input_address: tuple: Входной адрес для сравнения.
-        json_file_name: str : Путь к фалу (имя файла)
+        json_file_content: list : Содержимое JSON-файла
 
     Returns:
         str: Значение времени (times) в виде строки, если адрес найден, иначе пустая строка.
     """
 
+    # pattern = street
     street = input_address[0]
-    pattern = street
-    print(pattern)
-    try:
-        with open(json_file_name, "r", encoding="utf-8") as json_file:
-            data = json.load(json_file)
-        for item in data:
-            print(item)
-            times = item.get("times")
-            street_match = re.search(street, str(item.values()))
-            house_match = any(input_address[1] in x for x in item.get("houses"))
-            date_match = re.search(str(date.today()), str(item.get("times")))
-            print(str(date.today()), str(item.get("times")))
-            if street_match and house_match and date_match:
 
-                print("Адрес, дом, дата - ОК")
-                # Адрес найден, возвращаем значение times как строку
-                time_str = f"{times[0][0][11:16]}-{times[0][1][11:16]}"
+    for item in json_file_content:
 
-                return "Плановые работы СЭ: "+time_str
+        times = item.get("times")
+        street_match = re.search(street, str(item.values()))
+        house_match = any(input_address[1] in x for x in item.get("houses"))
+        date_match = re.search(str(date.today()), str(item.get("times")))
 
-            # Адрес не найден
-            print("Адрес не найден")
-            return ""
-    except FileNotFoundError:
-        return "Ошибка: JSON-файл не найден."
-    except json.JSONDecodeError:
-        return "Ошибка: Неверный формат JSON-файла."
+        if street_match and house_match and date_match:
+            # Адрес найден, возвращаем значение times как строку
+            print(f"{input_address} Адреc, дом, дата - ОК")
+            time_str = f"{times[0][0][11:16]}-{times[0][1][11:16]}"
+
+            return "Плановые работы СЭ: "+time_str
+
+        # Адрес не найден
+        continue
+    return ""
+
 
 
 
--- tests/test_has_outages.py
+++ tests/test_has_outages.py
@@ -2,7 +2,7 @@ import json
 from datetime import datetime, date
 from unittest import TestCase
 
-from app.has_outages import has_outages
+from app.has_outages import has_outages, json_opener
 
 
 class TestAddressToVerboseConverter(TestCase):
@@ -41,7 +41,83 @@ class TestAddressToVerboseConverter(TestCase):
             outfile.write(json_string)
 
 
-        address_from_zabbix = ("Курчатова","1")
+        address_from_zabbix = ("Вакуленчука","22")
+        valid = "Плановые работы СЭ: 08:00-16:00"
+
+
+        json_file_content = json_opener(json_file_name)
+
+        self.assertEqual(valid, has_outages(address_from_zabbix, json_file_content))
+
+    def test_has_outages_village(self):
+
+        #  Создание файла json
+        #  Название файла
+        json_file_name = f"data-{date.today()}.json"
+        #  Содержимое файла
+        list_of_outages =   [           {
+                "address": "с. Терновка, ул. Ленина",
+                "houses": [
+                    "2",
+                    "6",
+                    "8",
+                    "10",
+                    "12",
+                    "14",
+                    "16",
+                    "18",
+                    "142",
+                    "7",
+                    "9",
+                    "11",
+                    "13",
+                    "15",
+                ],
+                "times": [
+                    [
+                        "2024-02-21 08:00:00",
+                        "2024-02-21 16:00:00"
+                    ]
+                ],
+            },
+            {
+                "address": "с. Терновка, ул. Балаклавская",
+                "houses": [
+                    "2",
+                    "4",
+                    "6",
+                    "8",
+                    "10",
+                    "12",
+                    "14",
+                    "16",
+                    "1б",
+                    "1",
+                    "3",
+                    "5",
+                    "7",
+                    "9",
+                    "11",
+                    "13",
+                    "15",
+                    "17",
+                ],
+                "times": [
+                    [
+                        "2024-02-21 08:00:00",
+                        "2024-02-21 16:00:00"
+                    ]
+                ],
+            }
+        ]
+
+        #  Запись в файл
+        json_string = json.dumps(list_of_outages, indent=2, ensure_ascii=False, default=str)
+        with open(json_file_name, "w", encoding="utf-8") as outfile:
+            outfile.write(json_string)
+
+
+        address_from_zabbix = (r'Терновка(.+?)Ленина', '2')
         valid = "Плановые работы СЭ: 08:00-16:00"
 
         self.assertEqual(valid, has_outages(address_from_zabbix, json_file_name))
